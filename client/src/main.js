56e44bea293e38e2af9ab78d9aee454e5cc88786
the80hz <dnl.vlgv@mail.ru>
2026-05-05 16:33:12 +0400

    Show all GeoServer layers in client

--- client/src/main.js
+++ client/src/main.js
@@ -8,23 +8,30 @@ import ImageWMS from 'ol/source/ImageWMS';
 import { fromLonLat } from 'ol/proj';
 import './style.css';
 
+const wmsUrl = 'http://localhost:8080/geoserver/gis/wms';
+
+const createWmsLayer = (layerName) =>
+  new ImageLayer({
+    source: new ImageWMS({
+      url: wmsUrl,
+      params: {
+        LAYERS: layerName,
+        TILED: true,
+      },
+      ratio: 1,
+      serverType: 'geoserver',
+    }),
+  });
+
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
       source: new OSM(),
     }),
-    new ImageLayer({
-      source: new ImageWMS({
-        url: 'http://localhost:8080/geoserver/gis/wms',
-        params: {
-          LAYERS: 'gis:buildings',
-          TILED: true,
-        },
-        ratio: 1,
-        serverType: 'geoserver',
-      }),
-    }),
+    createWmsLayer('gis:buildings'),
+    createWmsLayer('gis:roads'),
+    createWmsLayer('gis:poi'),
   ],
   view: new View({
     center: fromLonLat([50.1287, 53.524]),
